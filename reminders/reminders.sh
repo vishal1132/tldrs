024605474180f2a2a714b3ce02b50cbd17b28f71
vishal1132 <vishals@iitbhilai.ac.in>
2022-11-23 17:27:42 +0530

    fix shell script for reminders to exclude ##

--- reminders/reminders.sh
+++ reminders/reminders.sh
@@ -1,8 +1,8 @@
 quotes_file=${1:-/Users/vishal/work/src/github.com/vishal1132/tldrs/reminders/reminders.md} # path to reminders.md
-total_quotes=$(cat $quotes_file | sed '/^$/d' | wc -l)
+total_quotes=$(cat $quotes_file | sed '/^$/d' | sed '/^#/d' | wc -l)
 export total_quotes=$total_quotes 
 random_number=$(perl -e 'print(int(rand($ENV{total_quotes}))+1)')
-quote=$(cat $quotes_file | sed '/^$/d' |awk "NR==$random_number")
+quote=$(cat $quotes_file | sed '/^$/d' | sed '/^#/d' | awk "NR==$random_number")
 quote_title=$(echo $quote | awk -F ";" '{print $1}')
 quote_body=$(echo $quote | awk -F ";" '{print $2}')
 terminal-notifier -title reminder -subtitle "$quote_title" -message "$quote_body" -sound Glass -group reminders -execute "say $quote_body -r 200 -v Tessa"
